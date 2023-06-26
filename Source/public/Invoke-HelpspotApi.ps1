e3e64c21b9cb55fb296802b5bde7b1f73d59a38e
trackd <trackd@users.noreply.github.com>
2023-06-26 02:06:12 +0200

    testing stuff

--- Source/public/Invoke-HelpspotApi.ps1
+++ Source/public/Invoke-HelpspotApi.ps1
@@ -21,34 +21,42 @@
         [String]
         $apikey
     )
-    $headers = @{
-        Authorization = "Bearer $apikey"
-    }
-    $config = $script:apidefinition[$ApiEndpoint]
-    if ($Params) {
-        $ExtraParamString = [System.Web.HttpUtility]::ParseQueryString('')
-        foreach ($object in $Params.GetEnumerator()) {
-            if ($config.param[$object.Name]) {
-                $ExtraParamString.Add("$($object.Name)", "$($object.Value)")
-                $config.param.remove[$object.Name]
-            } else {
-                Write-Warning "$($object.Name) is not a valid parameter for this endpoint, only: `n$($script:apidefinition[$ApiEndpoint].param.keys -join ', ')"
+    begin {
+        $headers = @{
+            Authorization = "Bearer $apikey"
+        }
+        $config = $script:apidefinition[$ApiEndpoint].clone()
+        if ($Params) {
+            $ExtraParamString = [System.Web.HttpUtility]::ParseQueryString('')
+            foreach ($object in $Params.GetEnumerator()) {
+                if ($config.param[$object.Name]) {
+                    $ExtraParamString.Add("$($object.Name)", "$($object.Value)")
+                    $config.param.remove($object.Name)
+                } else {
+                    Write-Warning "$($object.Name) is not a valid parameter for this endpoint, only: `n$($script:apidefinition[$ApiEndpoint].param.keys -join ', ')"
+                }
             }
-        }
-        $urlBuilder = "$($baseUrl)/api/index.php?method=$($ApiEndpoint)&$($ExtraParamString.Tostring())&output=json"
-    } else {
-        $urlBuilder = "$($baseUrl)/api/index.php?method=$($ApiEndpoint)&output=json"
-    }
-    Write-Debug "Method: $Method Url: $urlBuilder"
-    try {
-        $RequiredParameters = $config.param.GetEnumerator() | Where-Object { $_.Value.Required -eq $true }
-        if (-Not $RequiredParameters) {
-            $response = Invoke-RestMethod -Headers $headers -Uri $urlBuilder -Method $config.method
-            $response
+            $urlBuilder = "$($baseUrl)/api/index.php?method=$($ApiEndpoint)&$($ExtraParamString.Tostring())&output=json"
         } else {
-            Write-Error "Missing Parameters $($RequiredParameters.key -join ', ') `nGet-HelpspotAPIHelp -ApiEndpoint $($ApiEndpoint)" -ErrorAction Stop
+            $urlBuilder = "$($baseUrl)/api/index.php?method=$($ApiEndpoint)&output=json"
         }
-    } catch {
-        throw $_
+    }
+    process {
+        Write-Debug "Method: $($config.method) Url: $urlBuilder"
+        try {
+            $RequiredParameters = $config.param.GetEnumerator() | Where-Object { $_.Value.Required -eq $true }
+            if (-Not $RequiredParameters) {
+                $response = Invoke-RestMethod -Headers $headers -Uri $urlBuilder -Method $config.method
+                $response
+            } else {
+                Write-Debug "config: $($config.param.GetEnumerator() | Out-String )"
+                Write-Error "Missing Parameters $($RequiredParameters.key -join ', ') `nGet-HelpspotAPIHelp -ApiEndpoint $($ApiEndpoint)" -ErrorAction Stop
+            }
+        } catch {
+            throw $_
+        }
+    }
+    clean {
+        if ($config) { Remove-Variable config }
     }
 }
